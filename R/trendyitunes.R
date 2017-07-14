7c2ce0bbb78dab6daf7e90ec87e42131b7732b72
Ruxue Peng <rp2815@columbia.edu>
2017-07-14 11:42:19 -0400

    Update trendyitunes.R

--- R/trendyitunes.R
+++ R/trendyitunes.R
@@ -3,7 +3,6 @@ trendyitunes = function(){
   iTunes = fromJSON("https://rss.itunes.apple.com/api/v1/us/tv-shows/top-tv-episodes/25/non-explicit/json")
   pool = data.frame(Name = iTunes$feed$results$artistName, 
                     Detail = paste("Episode:",iTunes$feed$results$name), 
-                    category = iTunes$feed$results$primaryGenreName,
-                    from = "iTunes")
-  return(pool)
+                    category = iTunes$feed$results$primaryGenreName,from = "iTunes")
+  list(pool = as.character(paste(pool$Name,collapse = "//  ")))
 }
